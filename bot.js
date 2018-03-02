af2fcacf09b919485465aa4a48e2b31180ac14f5
Zandercross12 <tuoi.nguyen69@icloud.com>
2018-03-02 16:43:18 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,8 +30,8 @@ client.on("message", async message => {
   if (command === "info") {
   	var embed = new Discord.RichEmbed()
   	.setThumbnail(message.author.avatarURL)
-  	.addField("Bot for","Hollowed Discord Server" true)
-  	.addField("Made By:","@Zandercross12#7714" true)
+  	.addField("Bot for","Hollowed Discord Server", true)
+  	.addField("Made By:","@Zandercross12#7714", true)
   	.setColor(0x00FFFF)
   	message.channel.sendEmbed(embed);
   }
