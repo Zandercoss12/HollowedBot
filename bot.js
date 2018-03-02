d23af2e70ab29fc4488fecb2dbfa10c0ba715552
Zandercross12 <tuoi.nguyen69@icloud.com>
2018-03-02 14:34:25 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,53 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
 
+// Vars Here \\
+
+client.on('ready', () => {
+  client.user.setPresence({game: {name: "<>help for commands!", type: 0}});
+  console.log('I am ready!');
+});
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase(); 
+
+  if (command === "help") {
+  	message.author.sendMessage("```md\n[Help](HollowedBOT)\n[1]: help - Displays this help menu\n[2]: setpresence - Sets the <>help to something else\n- More will be added in the future.```");
+  	message.channel.sendMessage("`I just messaged you the commands! Go check them out! ` + message.author.toString()");
+  }
+
+  if (command === "setpresence") {
+  	const setPresence = args.join(" ");
+  	message.delete().catch(O_o=>{});
+  	client.user.setPresence({game: {name: setPresence, type: 0}});
+  }
+
+  if (command === "info") {
+  	var embed = new Discord.RichEmbed()
+  	.setThumbnail(message.client.avatarURL)
+  	.addField("Bot for Hollowed Discord Server")
+  	.addField("Made By: @Zandercross12#7714")
+  	.setColor(0x00FFFF)
+  	message.channel.sendEmbed(embed);
+  }
+
+  if (command === "author") {
+  	message.channel.sendMessage("<@257337698338078721> is the creator of this bot!");
+  }
+
+  if (command === "");
+
+});
+
+setInterval(() => {
+ http.get('http://discordjs-heroku.herokuapp.com');
+}, 900000);
+
+const port = process.env.PORT || 5000;
+
+client.login(process.env.BOT_TOKEN);
